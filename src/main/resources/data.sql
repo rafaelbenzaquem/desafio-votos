132431ff91f6b9163b21251a3cf8765664e29784
Rafael Benzaquem Neto <rafael.neto@outlook.com>
2022-08-20 13:58:28 -0300

    :tada: Commit inicial

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+CREATE SCHEMA IF NOT EXISTS desafio_votos_db;
---
+++ src/test/java/br/com/benzaquem/desafiovotos/DesafioVotosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.benzaquem.desafiovotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
